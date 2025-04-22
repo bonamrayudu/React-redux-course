13ff778fb5159030a5e05c7e6de0cb0d3927c0ea
Thrisurya Rayudu Bonam <bonamrayudu1999@gmail.com>
2025-04-22 19:15:26 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@
 import './App.css';
 
 function App() {
-  let number=Math.random()* 10
+  let numbers=Math.random()* 10
 let message= "Below 5"
   if (numbers>5){
   message="Above 5"
-  console.log(number);
+  console.log(numbers);
+  let date= new Date()
+  var time= date.toLocaleTimeString();
   
   }
   return (
     <div className="App">
       <h1>{message}</h1>
       <h1>{numbers}</h1>
+      <h1>{time}</h1>
     </div>
   );
 }
